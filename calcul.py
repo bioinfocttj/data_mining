1241c84e537697b848e2a8d1c87181ec8528cf8f
Typhaine Paysan Lafosse <tgauci@emi.u-bordeaux1.fr>
2012-11-16 16:12:31 +0100

    modif fichier calc pour enlever les tissue apparaissant plusieurs fois pour meme proteine

--- calcul.py
+++ calcul.py
@@ -4,13 +4,6 @@ file = open('echantillon_parse.xml','r')
 tree = etree.parse(file)
 root = tree.getroot()
 
-#------recup mots cles--------
-
-kw = ['Cell adhesion', 'Transcription regulation', 'Repeat', 'RNA-binding', 'Developmental protein', 'Early protein', 'Zinc-finger', 'Cell division', 'Viral envelope protein', 'Protease', 'Transcription', 'Differentiation', 'Phosphoprotein', 'Immunoglobulin domain', 'Cell cycle', 'Reference proteome', 'Complete proteome', 'Viral nucleoprotein', '3D-structure', 'Ubl conjugation', 'Mitochondrion', 'Viral attachment to host cell', 'Polymorphism', 'Lipid metabolism', 'Ion transport', 'Nucleotidyltransferase', 'Methylation', 'Transferase', 'Acetylation', 'Ribonucleoprotein', 'Palmitate', 'Activator', 'Viral penetration into host cytoplasm', 'Cell membrane', 'Coiled coil', 'Secreted', 'G-protein coupled receptor', 'Ubl conjugation pathway', 'Olfaction', 'AIDS', 'Signal', 'Immunity', 'Transport', 'Inhibition of host interferon signaling pathway by virus', 'Host cell membrane', 'Apoptosis', 'Fusion of virus membrane with host endosomal membrane', 'Transit peptide', 'Host membrane', 'Sensory transduction', 'Cytoplasm', 'Signal-anchor', 'Membrane', 'Fusion of virus membrane with host membrane', 'Cell junction', 'Oxidoreductase', 'Repressor', 'Virion', 'Late protein', 'Myristate', 'Cytoskeleton', 'Calcium', 'Glycoprotein', 'Serine/threonine-protein kinase', 'Cell projection', 'Viral immunoevasion', 'Capsid protein', 'Nucleotide-binding', 'Protein transport', 'mRNA processing', 'Magnesium', 'Ionic channel', 'Receptor', 'Metal-binding', 'Transmembrane', 'DNA replication', 'Host cytoplasm', 'ATP-binding', 'Disease mutation', 'Viral RNA replication', 'Golgi apparatus', 'Kinase', 'Isopeptide bond', 'Transmembrane helix', 'Transducer', 'Inhibition of host innate immune response by virus', 'Disulfide bond', 'Zinc', 'Nucleus', 'Alternative splicing', 'Direct protein sequencing', 'Lipoprotein', 'Host nucleus', 'Hydrolase', 'Cleavage on pair of basic residues', 'Virus entry into host cell', 'Host-virus interaction', 'DNA-binding', 'Endoplasmic reticulum']
-
-
-
-
 
 # -------calul nb alpha beta----------
 nb_strand = 0	
@@ -38,13 +31,18 @@ for parent in tree.getiterator():
 		turn.attrib["nb"] = str(nb_turn)
 		parent.append(turn)
 
-# ------supprime les mots cles non selectionnes-----		
-for node in tree.getiterator():
-	if node.tag=='keyword':
-		if node.text not in kw :
-			#print node.text
-			node.getparent().remove(node)
+# ------suppression tissue appaissant plusieurs fois pour une meme proteine-----	
+	
+for parent in tree.getiterator():
+    if parent.tag=='entry':
+        tissue=[]
+        for child in parent:
+            if child.tag=='tissue':
+                if child.text not in tissue :
+                    tissue.append(child.text)
+                else:
+                    child.getparent().remove(child)
 			
 
-tree.write("Test.xml")
+tree.write("echcalc.xml")
 
