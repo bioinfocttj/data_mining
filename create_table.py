ee3271837ea53c26d192ae77d1e3b56f25e75781
bioinfocttj <typhaine.pl@live.fr>
2012-11-21 15:43:19 +0100

    dico et remplissage matrice :)

--- create_table.py
+++ create_table.py
@@ -1,7 +1,6 @@
 import lxml.etree as etree #parse XML
 import math
-from scipy import cluster
-from matplotlib.pyplot import show
+
 
 file = open('echcalc.xml','r')
 
@@ -17,42 +16,48 @@ for parent in tree.findall('.//entry'):
 	for child in parent :
 		if child.tag == 'accession':
 			id_prot = child.text
-			dico_prot[id_prot]=[0,0,0,0]
-			dico_struct[id_prot]=[0,0,0,0,0,0,0]
-			dico_chimie[id_prot]=[0,0]
+			dico_prot[id_prot]={'fullname':0,'gene':0,'length':0,'tissue':0}
+			dico_struct[id_prot]={'nb_cystein':0,'nb_strand':0,'pct_strand':0,'nb_helix':0,'pct_helix':0,'nb_turn':0,'pct_turn':0}
+			dico_chimie[id_prot]={'gravy':0,'phi':0}
 		if child.tag=='fullName':
-			dico_prot[id_prot][0] = child.text
+			dico_prot[id_prot]['fullname'] = child.text
 		if child.tag=='tissue':
 			list_tissue.append(child.text)
 		if child.tag =='sequence':
-			dico_prot[id_prot][2]=float(child.attrib['length'])
-			dico_chimie[id_prot][0]=float(child.attrib['gravy'])
-			dico_chimie[id_prot][1]=float(child.attrib['phi'])
-			dico_struct[id_prot][0]=float(child.attrib['cysteine'])
+			dico_prot[id_prot]['length']=float(child.attrib['length'])
+			dico_chimie[id_prot]['gravy']=float(child.attrib['gravy'])
+			dico_chimie[id_prot]['phi']=float(child.attrib['phi'])
+			dico_struct[id_prot]['nb_cystein']=float(child.attrib['cysteine'])
 		if child.tag == 'gene':
 			nb_gene+=1
 			if len(child.getchildren())>0:
-				dico_prot[id_prot][1]=child.getchildren()[0].text
+				dico_prot[id_prot]['gene']=child.getchildren()[0].text
 			else :
-				dico_prot[id_prot][1]='NOGENE'	
+				dico_prot[id_prot]['gene']='NOGENE'
 		if child.tag == 'strand' :
-			dico_struct[id_prot][1]=float(child.attrib['nb'])
-			dico_struct[id_prot][2]=float(child.attrib['pct'])
+			dico_struct[id_prot]['nb_strand']=float(child.attrib['nb'])
+			dico_struct[id_prot]['pct_strand']=float(child.attrib['pct'])
 		if child.tag == 'helix' :
-			dico_struct[id_prot][3]=float(child.attrib['nb'])
-			dico_struct[id_prot][4]=float(child.attrib['pct'])
+			dico_struct[id_prot]['nb_helix']=float(child.attrib['nb'])
+			dico_struct[id_prot]['pct_helix']=float(child.attrib['pct'])
 		if child.tag == 'turn' :
-			dico_struct[id_prot][5]=float(child.attrib['nb'])
-			dico_struct[id_prot][6]=float(child.attrib['pct'])
+			dico_struct[id_prot]['nb_turn']=float(child.attrib['nb'])
+			dico_struct[id_prot]['pct_turn']=float(child.attrib['pct'])
 	if nb_gene == 0 :
-		dico_prot[id_prot][1]='NOGENE'
-	dico_prot[id_prot][3]=list_tissue
+		dico_prot[id_prot]['gene']='NOGENE'
+	dico_prot[id_prot]['tissue']=list_tissue
 
 
 
 # dico_prot = {id_prot : [fullname,gene,length,[list tissue]]}
 # dico_struct = {id_prot : [nb_cystein,nb_strand,pct_strand,nb_helix,pct_helix,nb_turn,pct_turn]}
 # dico_chimie = {id_prot : [gravy,phi]}
+
+
+
+
+
+#---------- Calcul matrice de distance length + cluster -----------------
 
 def init_matrix(d):   #creation matrice de distance de dimension d
 	matrix=[]
@@ -81,25 +86,56 @@ def reduct_matrix(matrix):
     else:
         red_matrix=matrix
     return red_matrix 
-    
+
 def dist_length(a,b): # calcul distance 
 	dist = math.fabs(a-b)
 	return dist
 
-matrix_length = init_matrix(len(dico_prot))
-list_id = []  #pour garder en memoire l'ordre des proteines dans la matrice
 
-for key in dico_prot :
-	list_id.append(key)
+matrix_length = init_matrix(len(dico_prot))
+
+#---- remplissage de la matrice ----
 i=0
-j=0		
-for i in range (len(matrix_length)):
-	a = dico_prot[list_id[i]][2]
-	for j in range (len(matrix_length)):
-		b = dico_prot[list_id[j]][2]
+j=0
+for key1 in dico_prot.keys():
+	a = dico_prot[key1]['length']
+	for key2 in dico_prot.keys():
+		b = dico_prot[key2]['length']
 		matrix_length[i][j] = dist_length(a,b)
-		
+		j+=1
+	i+=1
+	j=0
+	
+
+
 matrix_red = reduct_matrix(matrix_length)	
 
-print matrix_red
+#---- tests ----
+"""
+step_cluster =[] #contient toutes les etapes de clusterisation
+cluster = [] # cluster en cours
 
+# toutes les proteines sont dans un seul cluster
+for key in dic_prot :
+	cluster.append(key)
+	
+step_cluster.append(cluster) #ajout premiere etape
+i=0
+j=0
+
+while  (len(cluster))!=0:
+	d=0
+	for key1 in dico_prot.keys():
+		for key2 in range dico_prot.keys():
+			j+=1
+			if matrix_red[i][j]>d
+			d = matrix_red[i][j]
+			p1_id = dico_prot[key1]
+			p2_id = dico_prot[key2]
+	i+=1
+	j=0
+"""
+
+
+
+matrix_red
