582252dbb5906d92201f7311b849182e29ff116f
bioinfocttj <typhaine.pl@live.fr>
2012-11-23 16:28:27 +0100

    mise à jour creation cluster, erreur à la création 2e matrice

--- create_table.py
+++ create_table.py
@@ -80,35 +80,89 @@ matrix_length = init_matrix(len(dico_prot)+1)
 
 #---- remplissage de la matrice ----
 
-ligne = 1
-for key in dico_prot.keys():#ajout des identifiants ds la 1ere ligne et 1ere colonne de la matrice de distance
-	matrix_length[0][ligne]=key
-	matrix_length[ligne][0]=key
-	ligne+=1
 
-for i in range (1,len(matrix_length)):
-	key1=matrix_length[0][i]
-	a = dico_prot[key1]['length']
-	for j in range (1,len(matrix_length)):
-		key2=matrix_length[j][0]
-		b = dico_prot[key2]['length']
-		matrix_length[i][j] = dist_length(a,b)
 
-def min(matrix_length):
+def ajout_identifiant(cluster,matrix):
+	ligne = 1
+	for key in range(len(cluster)):#ajout des identifiants ds la 1ere ligne et 1ere colonne de la matrice de distance
+		matrix[0][ligne]=cluster[key]
+		matrix[ligne][0]=cluster[key]
+		ligne+=1
+	return matrix
+
+def remplir_matrice_initiale(dico_prot,matrix):
+	for i in range (1,len(matrix)):
+		key1=matrix[0][i]
+		a = dico_prot[key1[0]]['length']
+		for j in range (1,len(matrix)):
+			key2=matrix[j][0]
+			b = dico_prot[key2[0]]['length']
+			matrix[i][j] = dist_length(a,b)
+	return matrix
+
+def mini(matrix_length): #plus petite distance dans la matrice
 	savI=1
 	savJ=2
 	x=2
 	mini=matrix_length[1][2]
 	for i in range (x,len(matrix_length)):
 		for j in range (1,len(matrix_length)):
-			if matrix_length[i][j]<mini:
+			if matrix_length[i][j]<mini and matrix_length[i][j]!=0:
 				mini=matrix_length[i][j]
-				savI=i
-				savJ=j
+				savI=matrix_length[0][i]
+				savJ=matrix_length[j][0]
 		x+=1
 	return savI,savJ
 
-def matrice_distance(matrice,i,j):
+def remplir_matrice(matrice,i,j,cluster): #fusion valeurs i et j 
+	newcluster=new_cluster(cluster,i,j)
 	matrix = init_matrix(len(matrice)-1)
+	matrix = ajout_identifiant(newcluster,matrix)
 	for ligne in range (len(matrice)):
-		matrix[][]=(matrice[i][ligne]+matrice[j][ligne])/2
+		for k in range(len(matrix[0])):
+			if matrix[0][k]==matrice[0][ligne]:
+				for l in range (1,len(matrix)):
+#############################################################################################################################
+				#	print "test2",matrix[0][l]																				#
+				#	print "i",i[0]																							#
+					if (i[0]==matrix[0][l][0]):																				#
+						matrix[0][k]=(matrice[l][ligne]+matrice[l][ligne])/2#calcul ou if foireux, matrice resultat vide	#
+						matrix[k][0]=(matrice[l][ligne]+matrice[l][ligne])/2												#
+	return matrix																											#
+																															#
+#############################################################################################################################
+def liste_cluster(listeCluster,cluster):
+	listeCluster.append(cluster)
+
+
+def creer_cluster(dico_prot):
+	cluster=[]
+	#cluster=[len(dico_prot)]
+	for key in dico_prot.keys():
+	#	for i in range(len(cluster)):
+		temp=[key]
+		cluster.append(temp)
+	return cluster
+			
+def new_cluster(cluster,i,j):
+	newCluster=[]
+	for k in range (len(cluster)):
+		if cluster[k]!=i and cluster[k]!=j:
+			newCluster.append(cluster[k])
+	temp=[]
+	temp.append(i)
+	for k in range (len(i)):
+		temp.append(i[k])
+	for k in range (len(j)):
+		temp.append(j[k])
+	newCluster.append(temp)
+	return newCluster
+	
+clust1=creer_cluster(dico_prot)
+#print clust1
+mat=ajout_identifiant(clust1,matrix_length)
+mat=remplir_matrice_initiale(dico_prot,mat)
+savI,savJ=mini(mat)
+mat2=remplir_matrice(mat,savI,savJ,clust1)
+print mat2
+
